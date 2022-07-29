f488018d3708edc532fb210adb9c98b7d91f03f2
Paul Schmeing <imveenix@gmail.com>
2022-07-29 19:57:25 +0200

    added examples for basic "asm"

---
+++ examples/basic_operations.s
@@ -0,0 +1,19 @@
+; Addition
+MOV R1, 13
+MOV R2, 7
+ADD R1, R2
+MOV R3, ACC
+
+; Substraction
+MOV R1, 20
+MOV R2, 10
+SUB R1, R2
+MOV R4, ACC
+
+; Multiplication
+MUL R3, R4
+MOV R3, ACC
+
+; Division
+DIV R3, 10
+MOV R4, ACC
